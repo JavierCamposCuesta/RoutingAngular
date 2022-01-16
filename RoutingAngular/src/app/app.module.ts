4b30c473a8d152a9f139c01306690c1c52dc2039
JavierCamposCuesta <javiercamposcuesta@gmail.com>
2022-01-16 13:08:40 +0100

    Terminado proyecto Rutas

---
+++ RoutingAngular/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UsersModule } from './users/users.module';
+import { ServersModule } from './servers/servers.module';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AuthGuard } from './auth-guard.service';
+import { AuthService } from './auth.service';
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
+import { ErrorPageComponent } from './error-page/error-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    ErrorPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    UsersModule,
+    ServersModule
+  ],
+  providers: [
+    AuthGuard,
+    AuthService,
+    CanDeactivateGuard
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
